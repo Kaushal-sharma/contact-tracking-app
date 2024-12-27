c96ea3e3afb061876381ae2d54d32166da3292c8
kaushal-sharma <kaushal.dev0001@gmail.com>
2024-12-27 23:12:49 +0530

    Create ctHealthHeader child component

--- force-app/main/default/lwc/cTHealthAdminView/cTHealthAdminView.js
+++ force-app/main/default/lwc/cTHealthAdminView/cTHealthAdminView.js
@@ -1,58 +1,20 @@
 import { LightningElement, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getPicklistValues } from "lightning/uiObjectInfoApi";
-import HEALTH_STATUS from "@salesforce/schema/Person__c.Health_Status__c";
-
-import getHealthStatus from '@salesforce/apex/CTPersonController.getHealthStatusCount';
 
 export default class CTHealthAdminView extends LightningElement {
 
     tabName = '';
-    @track healthStatus = [];
-    picklist=[];
-
-    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: HEALTH_STATUS })
-    picklistResults({ error, data }) {
-        if (data) {
-          this.picklist = data.values;
-          this.error = undefined;
-        } else if (error) {
-          this.error = error;
-          this.ratings = undefined;
-        }
-    }
-
-    connectedCallback() {
-        this.retrieveHealthStatus();
-    }
-
-    retrieveHealthStatus() {
-        getHealthStatus()
-        .then((response) => {
-            this.picklist.forEach((item, index) => {
-                for(let [key, value] of Object.entries(response)) {
-                    if(item.label == key) {
-                        item = {...item, count:value, color:`text-${item.label}`}
-                    } 
-                }
-                if(!Object.hasOwn(item, 'count')) {
-                    item = {...item, count:0, color:`text-${item.label}`}
-                }
-                this.healthStatus.push(item);
-            });
-        })
-        .catch((error) => {
-            this.showMessage(error.statusText, error, 'error');
-        })
-    }
-
-    createNewRecord(event) {
-        console.log(`Create ${this.tabName} record.`);
-    }
 
     handleActive(event) {
         const name = event.target.value;
         this.tabName = name.charAt(0).toUpperCase() + String(name).slice(1);
+        if(this.tabName == 'Location') {
+            this.template.querySelector('c-c-t-health-header').getPersonStatus();
+        } else {
+            this.template.querySelector('c-c-t-health-header').getLocationStatus();
+        }
+
+
     }
 
     showMessage(title, msg, variant) {
---
+++ force-app/main/default/lwc/cTHealthHeader/cTHealthHeader.js
@@ -0,0 +1,92 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+import HEALTH_STATUS from "@salesforce/schema/Person__c.Health_Status__c";
+import getPersonHealthStatus from '@salesforce/apex/CTHealthHeaderController.getPersonHealthStatuscount';
+import getLocationHealthStatus from '@salesforce/apex/CTHealthHeaderController.getLocationHealthStatuscount';
+
+export default class CTHealthHeader extends NavigationMixin(LightningElement) {
+
+    @api tabName;
+    @track healthStatus = [];
+    picklist=[];
+
+    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: HEALTH_STATUS })
+    picklistResults({ error, data }) {
+        if (data) {
+          this.picklist = data.values;
+          this.error = undefined;
+        } else if (error) {
+          this.error = error;
+          this.ratings = undefined;
+        }
+    }
+
+    connectedCallback() {
+        //this.getPersonStatus();
+    }
+
+    @api
+    getPersonStatus() {
+        getPersonHealthStatus()
+        .then((response) => {
+            this.picklist.forEach((item, index) => {
+                for(let [key, value] of Object.entries(response)) {
+                    if(item.label == key) {
+                        item = {...item, count:value, color:`text-${item.label}`}
+                    } 
+                }
+                if(!Object.hasOwn(item, 'count')) {
+                    item = {...item, count:0, color:`text-${item.label}`}
+                }
+                this.healthStatus.push(item);
+            });
+        })
+        .catch((error) => {
+            this.showMessage(error.statusText, error, 'error');
+        })
+    }
+
+    @api
+    getLocationStatus() {
+        getLocationHealthStatus()
+        .then((response) => {
+            this.picklist.forEach((item, index) => {
+                for(let [key, value] of Object.entries(response)) {
+                    if(item.label == key) {
+                        item = {...item, count:value, color:`text-${item.label}`}
+                    } 
+                }
+                if(!Object.hasOwn(item, 'count')) {
+                    item = {...item, count:0, color:`text-${item.label}`}
+                }
+                this.healthStatus.push(item);
+            });
+        })
+        .catch((error) => {
+            this.showMessage(error.statusText, error, 'error');
+        })
+    }
+
+    createNewRecord(event) {
+        console.log(`Create ${this.tabName} record.`);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: this.tabName+'__c',
+                actionName: 'new'
+            },
+        });
+    }
+
+    showMessage(title, msg, variant) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: msg,
+                variant: variant
+            })
+        )
+    }
+}
