94e3830c850ab1ecc19dae759b81984ec6e9be6a
Kaushal-sharma <kaushal.dev0001@gmail.com>
2025-01-04 09:24:03 +0530

    remove getPicklistValues & hardcoded status label

--- force-app/main/default/lwc/healthHeader/healthHeader.js
+++ force-app/main/default/lwc/healthHeader/healthHeader.js
@@ -1,44 +1,20 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getPicklistValues } from "lightning/uiObjectInfoApi";
-import HEALTH_STATUS from "@salesforce/schema/Person__c.Health_Status__c";
 import getPersonHealthStatus from '@salesforce/apex/CTHealthHeaderController.getPersonHealthStatuscount';
 import getLocationHealthStatus from '@salesforce/apex/CTHealthHeaderController.getLocationHealthStatuscount';
 
 export default class HealthHeader extends NavigationMixin(LightningElement) {
 
     @api tabName;
-    @track healthStatus = [];
-    picklist=[];
+    statusInfo={};
 
-    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: HEALTH_STATUS })
-    picklistResults({ error, data }) {
-        if (data) {
-          this.picklist = data.values;
-          this.error = undefined;
-        } else if (error) {
-          this.error = error;
-          this.ratings = undefined;
-        }
-    }
 
     @api
     getPersonStatus() {
-        this.healthStatus=[];
         getPersonHealthStatus()
         .then((response) => {
-            this.picklist.forEach((item, index) => {
-                for(let [key, value] of Object.entries(response)) {
-                    if(item.label == key) {
-                        item = {...item, count:value, color:`text-${item.label}`}
-                    } 
-                }
-                if(!Object.hasOwn(item, 'count')) {
-                    item = {...item, count:0, color:`text-${item.label}`}
-                }
-                this.healthStatus.push(item);
-            });
+            this.statusInfo = response;
         })
         .catch((error) => {
             this.showMessage(error.statusText, error, 'error');
@@ -47,20 +23,10 @@ export default class HealthHeader extends NavigationMixin(LightningElement) {
 
     @api
     getLocationStatus() {
-        this.healthStatus = [];
         getLocationHealthStatus()
         .then((response) => {
-            this.picklist.forEach((item, index) => {
-                for(let [key, value] of Object.entries(response)) {
-                    if(item.label == key) {
-                        item = {...item, count:value, color:`text-${item.label}`}
-                    } 
-                }
-                if(!Object.hasOwn(item, 'count')) {
-                    item = {...item, count:0, color:`text-${item.label}`}
-                }
-                this.healthStatus.push(item);
-            });
+            this.statusInfo = response;
+            console.log(this.statusInfo);
         })
         .catch((error) => {
             this.showMessage(error.statusText, error, 'error');
@@ -76,6 +42,10 @@ export default class HealthHeader extends NavigationMixin(LightningElement) {
                 actionName: 'new'
             },
         });
+    }
+
+    refreshAllData() {
+        
     }
 
     showMessage(title, msg, variant) {
